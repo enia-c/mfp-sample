ebdac3775de5ed4d7e6c56581f80c739c281e3c1
enia.c <enia.0x63@gmail.com>
2024-03-31 08:54:40 +0900

    added auth subapp

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,23 +2,38 @@ import {
   StylesProvider,
   createGenerateClassName,
 } from "@material-ui/core/styles";
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import React, { Suspense, lazy, useState } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Header from "./components/Header";
-import MarketingApp from "./components/MarketingApp";
+import Progress from "./components/Progress";
+
+const MarketingApp = lazy(() => import("./components/MarketingApp"));
+const AuthApp = lazy(() => import("./components/AuthApp"));
 
 const generateClassName = createGenerateClassName({
   productionPrefix: "co",
 });
 
 export default () => {
+  const [isSignedIn, setIsSignedIn] = useState(false);
+
   return (
     <BrowserRouter>
       <StylesProvider generateClassName={generateClassName}>
         <div>
-          <Header />
-          <MarketingApp />
+          <Header
+            onSignOut={() => setIsSignedIn(false)}
+            isSignedIn={isSignedIn}
+          />
+          <Suspense fallback={<Progress />}>
+            <Switch>
+              <Route path="/auth">
+                <AuthApp onSignIn={() => setIsSignedIn(true)} />
+              </Route>
+              <Route path="/" component={MarketingApp} />
+            </Switch>
+          </Suspense>
         </div>
       </StylesProvider>
     </BrowserRouter>
--- packages/container/src/components/AuthApp.js
+++ packages/container/src/components/AuthApp.js
@@ -1,12 +1,13 @@
-import { mount } from "marketing/MarketingApp";
+import { mount } from "auth/AuthApp";
 import React, { useEffect, useRef } from "react";
 import { useHistory } from "react-router-dom";
 
-export default () => {
+export default ({ onSignIn }) => {
   const ref = useRef(null);
   const history = useHistory();
 
   useEffect(() => {
+    console.log("AuthAPp.js", history.location.pathname);
     const { onParentNavigate } = mount(ref.current, {
       initialPath: history.location.pathname,
       onNavigate: ({ pathname: nextPathname }) => {
@@ -14,6 +15,9 @@ export default () => {
         if (nextPathname !== pathname) {
           history.push(nextPathname);
         }
+      },
+      onSignIn: () => {
+        onSignIn();
       },
     });
     console.log(onParentNavigate);
